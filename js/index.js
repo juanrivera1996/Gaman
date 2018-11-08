29e82b5510e68f02c6124ccb083516e0a26c084b
juanrivera1996 <juanrivera1996@users.noreply.github.com>
2018-11-07 22:15:50 -0500

    edicion del html y css
    Se añadio al proyecto todos los recursos graficos, el codigo en estos momentos se encuentra separado por secciones ,la navegabilidad aun no esta disponible.

---
+++ js/index.js
@@ -0,0 +1,8 @@
+window.onload=start;
+function start() {
+    var btnMatchGame=document.getElementById("btnMatchGame");
+    var btnMemory=document.getElementById("btnMemory");
+    var btnCredits=document.getElementById("btnCredits");
+}
+
+
